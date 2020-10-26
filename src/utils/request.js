671c84a6dc0f00815ab2e354d0b7bdad6c40fe50
samanthatandrii <samantha.tandri@gmail.com>
2020-10-26 14:50:20 +0700

    init integration home

---
+++ src/utils/request.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+// create an axios instance
+const service = axios.create({
+  baseURL: process.env.VUE_APP_BASE_API,
+  // url = base url + request url
+  // withCredentials: true, // send cookies when cross-domain requests
+  timeout: 5000,
+  // request timeout
+});
+
+// request interceptor
+// service.interceptors.request.use(
+//   (config) => {
+//     // do something before request is sent
+//     // eslint-disable-next-line no-param-reassign
+//     config.headers['Content-Type'] = 'application/json';
+//     return config;
+//   },
+//   (error) => {
+//     // do something with request error
+//     console.log(error); // for debug
+//     return Promise.reject(error);
+//   },
+// );
+
+export default service;
