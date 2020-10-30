41edb413e815c17297675a14c369fddd0c28b74a
samanthatandrii <samantha.tandri@gmail.com>
2020-10-30 17:57:56 +0700

    fix test for rollcall

--- tests/e2e/specs/rollCall.js
+++ tests/e2e/specs/rollCall.js
@@ -19,19 +19,19 @@ describe('Roll Call Modal Test', () => {
     let dataLength = 0;
     cy.request('GET', 'https://dev.api.munlite.co/api/conference/5f96e22bdb7ee38458e581e9/delegates').then((response) => {
       dataLength = response.body.data;
-      for (let i = 0; i < dataLength; i += 1) {
-        cy.get('button').contains('Present').click();
+      if (dataLength.length > 0) {
+        for (let i = 0; i < dataLength.length; i += 1) {
+          cy.get('button').contains('Present').click();
+          cy.wait(850);
+        }
         cy.wait(850);
+        cy.get('button').contains('Continue').click();
+        cy.get('#select .selection:first-child .input').type(dataLength.length - 1);
+        cy.get('.input.red').type(1);
+        cy.get('button').contains('Pass').click();
+        cy.wait(1000);
+        cy.url().should('include', '/gsl');
       }
-      cy.wait(850);
-      cy.get('button').contains('Continue').click();
-      cy.get('#select .selection:first-child .input').type('4');
-      cy.get('.input.red').type('3');
-      cy.get('button').contains('Pass').click();
     });
-  });
-  it('Redirects to GSL', () => {
-    cy.wait(1000);
-    cy.url().should('include', '/gsl');
   });
 });
